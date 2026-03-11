609ca1e207824977fda29beff036742941196a49
jensz321 <jens@vanduinkerken.net>
2026-03-11 15:46:08 +0100

    building fix for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/jens-website/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
